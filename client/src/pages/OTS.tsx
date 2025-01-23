466868f58cd7de6b28ba6d1fc63220488a7d3843
Keaton Greer <keatongreer1@gmail.com>
2025-01-22 20:03:19 -0500

    Finished initial OTS page front-end

--- client/src/pages/OTS.tsx
+++ client/src/pages/OTS.tsx
@@ -1,5 +1,36 @@
+import OTS_Card from "@/components/OTS_Card";
+import { Stack } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_REPORTS } from "@/utils/queries";
+import { Machine } from "../models/Machine";
+
+interface OTSProps {
+  _id: string;
+  shiftNumber: string;
+  date: Date;
+  assignedUserId: string;
+  savedMachines: Machine[];
+}
+
 const OTS = () => {
-  return <section></section>;
+  const { loading, error, data: AllReportData } = useQuery(GET_ALL_REPORTS);
+  console.log(AllReportData);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <main className="flex-shrink-0">
+      <Stack gap="8" direction="row" wrap="wrap">
+        {AllReportData.getAllReports.map((data: OTSProps) => (
+          <OTS_Card
+            key={data._id}
+            date={new Date(data.date).toISOString().split("T")[0]}
+            shift={data.shiftNumber}
+            creator={data.assignedUserId}
+          />
+        ))}
+      </Stack>
+    </main>
+  );
 };
 
 export default OTS;
