f5c7533c9d6c01a0f28fced7ac7946a87f552177
Keaton Greer <keatongreer1@gmail.com>
2025-01-22 22:28:02 -0500

    Fixed file pathing for seeds

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,19 +1,29 @@
 import db from "../config/connection.js";
 import models from "../models/index.js";
 import cleanDB from "./cleanDb.js";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const { User, Report } = models;
 
-import userData from "./userData.json" assert { type: "json" };
-import reportData from "./reportData.json" assert { type: "json" };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function loadJson(filePath: string) {
+  const data = await fs.readFile(filePath, "utf-8");
+  return JSON.parse(data);
+}
 
 db.once("open", async () => {
   await cleanDB("User", "users");
   await cleanDB("Report", "reports");
 
+  const userData = await loadJson(path.resolve(__dirname, "userData.json"));
   await User.insertMany(userData);
   console.log("Users seeded!");
 
+  const reportData = await loadJson(path.resolve(__dirname, "reportData.json"));
   await Report.insertMany(reportData);
   console.log("Reports seeded!");
 
