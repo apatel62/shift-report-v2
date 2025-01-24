cb4a9e3d7d4647dd370772d5d5aa683bb75bf283
Arjun Patel <arjunpatel9217@gmail.com>
2025-01-23 21:43:57 -0600

    finished comments on server folder files

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -6,15 +6,16 @@ import { fileURLToPath } from "url";
 import path from "path";
 
 const { User, Report } = models;
-
+//defines the filename and directory name so the build seed command know which filename and directory to look for the json files of the mock data
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const jsonDirectory = path.resolve(__dirname, "../../src/seeds"); // or wherever the files are located after building
+const jsonDirectory = path.resolve(__dirname, "../../src/seeds"); 
 async function loadJson(filePath: string) {
   const data = await fs.readFile(filePath, "utf-8");
   return JSON.parse(data);
 }
-
+//opens the database once and cleans the models/collections first
+//then loads the json data into the corresponding collections
 db.once("open", async () => {
   await cleanDB("User", "users");
   await cleanDB("Report", "reports");
